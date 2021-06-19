3243fea79cd993bea0b561d57966b3abd8218318
codecamjam <camerontaghlabi@gmail.com>
2021-06-19 18:56:50 -0400

    lesson 53 handling post requests

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //   res
 //     .status(400)
@@ -25,6 +27,32 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      //201 means created
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+
+  //Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
+  // res.send('Done'); cant send 2 responses
 });
 
 const port = 3000;

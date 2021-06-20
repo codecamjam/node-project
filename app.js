059078ab27a624445a1df8d0dd97dfec7f660def
codecamjam <camerontaghlabi@gmail.com>
2021-06-19 22:44:07 -0400

    l66 serving static files

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 //1) MIDDLEWARE
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`)); //for serving static files
 
 app.use((req, res, next) => {
   console.log('hello from middleware');

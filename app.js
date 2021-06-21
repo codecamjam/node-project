7ad78c1f7b9590c04a7a24fa55d1e579249ab1b9
codecamjam <camerontaghlabi@gmail.com>
2021-06-21 17:30:33 -0400

    l88 another way of creating docs

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`)); //for serving static files
 
 app.use((req, res, next) => {
-  console.log('hello from middleware');
+  console.log('hello from middleware 👋');
   next();
 });
 
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: './config.env' }); //eslint-disable
+dotenv.config({ path: './config.env' });
+
+const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
-
-const app = require('./app');
 
 mongoose
   .connect(DB, {

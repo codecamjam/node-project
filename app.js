5c79cbfd76655a61b05dbc558a7db5d4d7bdee17
codecamjam <camerontaghlabi@gmail.com>
2021-06-23 12:21:32 -0400

    l113 implenting a global error handling middleware

--- app.js
+++ app.js
@@ -13,16 +13,6 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-//this is a catch all for unhandled routes. basically
-//if user types in invalid url, this is going to handle it
-//hence the all function (all http methods)
-app.all('*', (req, res) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`
-  });
-});
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
@@ -30,5 +20,44 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`
+  // });
+
+  //create an error then on the error object, add status and statusCode so
+  //our error handler can use them
+  const err = new Error(
+    `Can't find ${req.originalUrl} on this server!`
+  );
+
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  //IF THE NEXT FUNCTION HAS AN ARGUMENT, EXPRESS ASSUMES ITS AN ERROR
+  //IT WILL THEN SKIP ALL THE NEXT MIDDLEWARES IN THE MW STACK
+  //AND SEND THE ERROR TO THE GLOBAL ERROR HANDLER
+  next(err);
+});
+
+//express error handling middleware
+//error first function
+app.use((err, req, res, next) => {
+  //we dont know what status code cuz it could be
+  //many scenarios
+  //so we define the status code on the error
+  //could be error codes that we didnt make
+  //could come from other places in the code
+  //therefore we make a default error status code
+  err.statusCode = err.statusCode || 500; //internal server error 500
+  err.status = err.status || 'error'; //400/404 is fail, 500 server error
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  });
+});
 
 module.exports = app;

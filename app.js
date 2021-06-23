998e3a637d1bcd324aabdd99e7eedc2bbb954093
codecamjam <camerontaghlabi@gmail.com>
2021-06-23 12:51:36 -0400

    l114 better errors and refactoring

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -22,42 +24,21 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Can't find ${req.originalUrl} on this server!`
-  // });
+  // const err = new Error(
+  //   `Can't find ${req.originalUrl} on this server!`
+  // );
+  // err.status = 'fail';
+  // err.statusCode = 404;
+  // next(err);
 
-  //create an error then on the error object, add status and statusCode so
-  //our error handler can use them
-  const err = new Error(
-    `Can't find ${req.originalUrl} on this server!`
+  next(
+    new AppError(
+      `Can't find ${req.originalUrl} on this server!`,
+      404
+    )
   );
-
-  err.status = 'fail';
-  err.statusCode = 404;
-
-  //IF THE NEXT FUNCTION HAS AN ARGUMENT, EXPRESS ASSUMES ITS AN ERROR
-  //IT WILL THEN SKIP ALL THE NEXT MIDDLEWARES IN THE MW STACK
-  //AND SEND THE ERROR TO THE GLOBAL ERROR HANDLER
-  next(err);
 });
 
-//express error handling middleware
-//error first function
-app.use((err, req, res, next) => {
-  //we dont know what status code cuz it could be
-  //many scenarios
-  //so we define the status code on the error
-  //could be error codes that we didnt make
-  //could come from other places in the code
-  //therefore we make a default error status code
-  err.statusCode = err.statusCode || 500; //internal server error 500
-  err.status = err.status || 'error'; //400/404 is fail, 500 server error
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
---
+++ controllers/errorController.js
@@ -0,0 +1,13 @@
+//these are handlers (aka controllers)
+module.exports = (err, req, res, next) => {
+  //stack trace
+  console.log(err.stack); //will show where err happened
+
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  });
+};

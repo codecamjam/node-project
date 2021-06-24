21a8832f47dbef4712425e90b676a459fe76a0d9
codecamjam <camerontaghlabi@gmail.com>
2021-06-24 12:04:51 -0400

    l128 signing up users

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,45 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
+  //this is a security flaw
+  //we create a new user using the body
+  //this means user can specify role as admin
+  //this is bad serious security flaw
   const newUser = await User.create(req.body);
 
+  //THEREFORE THIS IS better
+  //prevents admin creation
+  //so we'd have to go into compass and edit admin manually
+  // const newUser = await User.create({
+  //   name: req.body.name,
+  //   email: req.body.email,
+  //   password: req.body.password,
+  //   confirmPassword: req.body.confirmPassword
+  // });
+
+  //get id from new user
+  //1 payload we put in our jwt
+  //2 next param is secret
+  //HSA256 encryption secret must be >=32 chars long
+  //longer the better
+  //token header will be created automatically
+  //3 options (session expiration date)
+  const token = jwt.sign(
+    { id: newUser._id }, //1
+    process.env.JWT_SECRET, //2
+    {
+      expiresIn: process.env.JWT_EXPIRES_IN //3
+    }
+  );
+  //all we need to do to login a new user
+  //bc user was created and right away
+  //we log user in by sending a token
+  //user client will store the token somehow someway
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser
     }
--- models/userModel.js
+++ models/userModel.js
@@ -31,8 +31,6 @@ const userSchema = new mongoose.Schema({
       //this means that whenever we update a user, we will have to
       //use save, not findOneAndUpdate like we did with tours
       validator: function(el) {
-        //we return either true or false
-        //true = valid
         return el === this.password;
       },
       message: 'Passwords are not the same'
@@ -40,33 +38,11 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-//encryption is gonna have between the moment we receieve the data and
-//before the moment where its saved to the db
 userSchema.pre('save', async function(next) {
-  //only encrypt if pw field has been updated
-  //only when pw is created or changed
-  //THIS IS THE CURRENT DOCUMENT (in this case the current user)
-  //if pw not modified, call next to move to next mw
   //JONAS COMMENT: ONLY RUN THIS FUNC IF PW WAS ACTUALLY MODIFIED
   if (!this.isModified('password')) return next();
-
-  //if it was we are going to hash the pw with BCRYPT algo
-  //first salt, then hash
-  //SALT the password - just means it
-  // adds a random string to the pw so that 2 equal passwords != the same hash
-  //the second argument is a cost parameter
-  //cost parameter - a measure of how cpu intensive this operation will be
-  //default value is 10 but 12 is better
-  //so basically the higher the cost, the better the pw will be encrypted
-  //this hash func is the async version of this hash function
-  //there's also a sync version but nah dawg
   //JONAS COMMENT: HASH THE PW WITH COST OF 12
   this.password = await bcrypt.hash(this.password, 12);
-
-  //we only needed this for validation of password typed in correctly twice
-  //this works even though passwordConfirm was required
-  //that only means it was a required input, not a required field:value pair
-  //to be persisted to the db
   //JONAS COMMMENT: DELETE PASSWORDCONFIRM FIELD
   this.passwordConfirm = undefined;
   next();

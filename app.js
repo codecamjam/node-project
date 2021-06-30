746c716f296747d5f5a3727dd1d46f89976cbb9b
codecamjam <camerontaghlabi@gmail.com>
2021-06-29 20:42:59 -0400

    l144 data sanitization

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -33,6 +35,35 @@ app.use('/api', limiter);
 //Body parser - basically reading data from body into req.body
 //we can also limit the amt of data coming from the body (limit: 10kb)
 app.use(express.json({ limit: '10kb' }));
+
+//SANITIZE THE DATA RIGHT AFTER BODY PARSER
+//Data sanitization against noswl query injection
+// {
+//   "email": {"$gt": ""},
+//   "password": "pass1234"
+// } /login THIS WOULD RETURN ADMIN!!!
+/* 
+This mw looks at req.body, req.query string, and req.params 
+and filters out all dollar signs and dots cuz thats how mongodb
+operators work so by removing them, they no longer work
+*/
+app.use(mongoSanitize());
+
+//data sanitization against XSS
+/* 
+this cleans any user input from malicious html code
+so imagine a hacker inserts bad html code with js attached
+then that would be injected into our html sight so that could be really bad
+{
+    "email": "tester@jonas.io",
+    "password": "real-password",
+    "passwordConfirm": "real-password",
+    "name": "<div id='bad-code'>Name</div>"
+}
+if posted in signup route,
+the name value would be converted to string so html cant be injected
+*/
+app.use(xss());
 
 //Serving static files
 app.use(express.static(`${__dirname}/public`));

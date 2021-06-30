0908afb30cdd437008b1289b0d6959f90156389c
codecamjam <camerontaghlabi@gmail.com>
2021-06-29 20:23:59 -0400

    l143 setting security http headers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -9,28 +10,34 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+//GLOBAL MIDDLEWARES
+
+//best to use helmet early in the mw stack
+//Set security http headers
+app.use(helmet());
+
+//Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-//this is middleware function
-
-//this helps prevent denial of service and brute force attacks
+//limit requests from same API
 const limiter = rateLimit({
-  //can define how many requests we allow in certain amt of time
-  //allow 100 requests from the same ip in one hour
   max: 100,
-  windowMs: 60 * 60 * 1000, //1hour
+  windowMs: 60 * 60 * 1000,
   message:
     'Too many requests from this IP, please try again in an hour!'
 });
-
-//this will affect all routes starting with api
 app.use('/api', limiter);
 
-app.use(express.json());
+//Body parser - basically reading data from body into req.body
+//we can also limit the amt of data coming from the body (limit: 10kb)
+app.use(express.json({ limit: '10kb' }));
+
+//Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+//Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();

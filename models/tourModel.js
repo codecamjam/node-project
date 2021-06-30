6f7334d8d7917febce47d7a00cb349e35f0840a1
codecamjam <camerontaghlabi@gmail.com>
2021-06-30 15:30:13 -0400

    l152 populating tour guides

--- models/tourModel.js
+++ models/tourModel.js
@@ -108,16 +108,10 @@ const tourSchema = new mongoose.Schema(
     ],
     guides: [
       {
-        //the expected type of each of the elements in the guides array to be a mongodb ID
         type: mongoose.Schema.ObjectId,
-        ref: 'User' //this is how we establish references between different data sets in mongoose
+        ref: 'User'
       }
     ]
-    /*  the idea is that tours and users will always remain separate entities in our db
-      so all we save in our tours document is user ids. then when we query the tour 
-      we want to automatically get access to tour guides but without the user docs being stored
-      in the tour document itself so we use referencing instead
-    */
   },
   {
     toJSON: { virtuals: true },
@@ -134,15 +128,18 @@ tourSchema.pre('save', function(next) {
   next();
 });
 
-//THIS WAS FOR EMBEDDING USER DOCUMENTS INTO TOURS DOCUMENT
-//but theres drawbacks in this situation so we are going to reference instead
-// tourSchema.pre('save', async function(next) {
-//   const guidesPromises = this.guides.map(
-//     async id => await User.findById(id)
-//   );
-//   this.guides = await Promise.all(guidesPromises);
-//   next();
-// });
+//populate aka fill up the guide references so it displays the documents in the output
+//even though only the references are in guides, not the actual user documents themselves
+//careful with populate because it performs a 2nd query so be mindful
+tourSchema.pre(/^find/, function(next) {
+  // this.populate('guides');
+  //this points to the current query
+  this.populate({
+    path: 'guides', //path to the references we want to populate
+    select: '-__v -passwordChangedAt' //hide these from output
+  });
+  next();
+});
 
 tourSchema.pre(/^find/, function(next) {
   this.find({ secretTour: { $ne: true } });

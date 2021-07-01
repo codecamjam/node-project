971cf136202efaba63b6d299231ba8e7f6d41b63
codecamjam <camerontaghlabi@gmail.com>
2021-06-30 23:03:00 -0400

    l164 adding missing authentication and authorization

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,39 +4,24 @@ const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
-router.get(
-  '/me',
-  authController.protect,
-  userController.getMe,
-  userController.getUser
-);
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-
 router.post('/forgotPassword', authController.forgotPassword);
-
 router.patch(
   '/resetPassword/:token',
   authController.resetPassword
 );
 
-router.patch(
-  '/updateMyPassword',
-  authController.protect,
-  authController.updatePassword
-);
+//this protects all routes that comes after this point
+router.use(authController.protect);
 
-router.patch(
-  '/updateMe',
-  authController.protect,
-  userController.updateMe
-);
+router.patch('/updateMyPassword', authController.updatePassword);
+router.get('/me', userController.getMe, userController.getUser);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
 
-router.delete(
-  '/deleteMe',
-  authController.protect,
-  userController.deleteMe
-);
+//from this point on, all the proceeding routes are admin only
+router.use(authController.restrictTo('admin'));
 
 router
   .route('/')

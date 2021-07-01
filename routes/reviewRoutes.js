edf50f2f37a49a0cccbf6248c7c4777da0eff757
codecamjam <camerontaghlabi@gmail.com>
2021-06-30 20:39:32 -0400

    l159 adding a nested get endpoint

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,14 +2,6 @@ const express = require('express');
 const reviewController = require('./../controllers/reviewController');
 const authController = require('./../controllers/authController');
 
-//to get tourId from tours/:tourId/reviews'
-//POST /tour/234fad4/reviews
-//GET /tour/234fad4/reviews
-//GET /tour/234fad4/reviews
-/*by default each router only has access to parameters of their own route
-but here in this route: /  there is no tour id but we still need access from
-the other router tours/:tourId/reviews' so we need to merge params  
-*/
 const router = express.Router({ mergeParams: true });
 
 router
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,28 +5,6 @@ const reviewRouter = require('./../routes/reviewRoutes');
 
 const router = express.Router();
 
-//this is the desired behavior aka a nested route
-//reviews is a child of tour
-//POST /tour/234fad4/reviews post a review of this tour
-//GET /tour/234fad4/reviews get all reviews of this tour
-//GET /tour/234fad4/reviews/sjdfhskdfh get specific review of the tour
-//Even though it is confusing that we have a review route in the tour router file,
-//we have to do this since the route starts with tour
-// router
-//   .route('/:tourId/reviews')
-//   .post(
-//     authController.protect,
-//     authController.restrictTo('user'),
-//     reviewController.createReview
-//   );
-
-/* 
-router itself is just mw so we can use the use method and 
-say for this specific route we should use reviewRouter instead
-this is just mounting the router again
-
-but we need to give reviewRouter access to tourId param
-*/
 router.use('/:tourId/reviews', reviewRouter);
 
 router

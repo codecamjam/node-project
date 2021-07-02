3713aa5d763464f8ee01283a19d36a8b7d572e9b
codecamjam <camerontaghlabi@gmail.com>
2021-07-01 22:27:49 -0400

    l181 building the tour overview part 1

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,19 @@
-exports.getOverview = (req, res) => {
+const Tour = require('./../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  //1 get all tour data from collection
+  const tours = await Tour.find();
+  console.log(tours);
+
+  //2 build template
+
+  //3 render that template using tour data from step 1
   res.status(200).render('overview', {
-    title: 'All Tours'
+    title: 'All Tours',
+    tours
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {

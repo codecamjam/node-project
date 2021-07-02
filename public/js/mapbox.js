c4861377ac74c649bc1bc956f1dc93c58611dca7
codecamjam <camerontaghlabi@gmail.com>
2021-07-02 17:02:39 -0400

    l186 including a map with mapbox

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -5,3 +5,49 @@ const locations = JSON.parse(
 );
 
 console.log(locations);
+
+mapboxgl.accessToken = process.env.MAPBOX_TOKEN; //this wont actually work
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: PROCESS.ENV.MAPBOX_STYLES,
+  scrollZoom: false
+  // center: [-118.113491, 34.111745],
+  // zoom: 10,
+  // interactive: false
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach(loc => {
+  //Create marker
+  let el = document.createElement('div');
+  el.className = 'marker';
+
+  //Add marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom'
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  //Add popup
+  new mapboxgl.Popup({
+    offset: 30
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  //Extend map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100
+  }
+});

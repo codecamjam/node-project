b51ab1302f7300d16dd27ff5abc6f694985645b7
codecamjam <camerontaghlabi@gmail.com>
2021-07-02 16:00:52 -0400

    l185 including a map with mapbox

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -17,7 +17,7 @@ exports.getTour = catchAsync(async (req, res, next) => {
     fields: 'review rating user'
   });
   res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
+    title: `${tour.name} Tour`,
     tour
   });
 });
---
+++ public/js/mapbox.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+
+const locations = JSON.parse(
+  document.getElementById('map').dataset.locations
+);
+
+console.log(locations);

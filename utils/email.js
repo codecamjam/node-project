d90b0679d40362b167a808fc0164f2703daac8e2
codecamjam <camerontaghlabi@gmail.com>
2021-07-14 18:05:08 -0400

    l206 email templates with pug: welcome emails

--- utils/email.js
+++ utils/email.js
@@ -31,7 +31,7 @@ module.exports = class Email {
   async send(template, subject) {
     //1 render HTML based on a pug template
     const html = pug.renderFile(
-      `${__dirname}/../views/emails/${template}.pug`,
+      `${__dirname}/../views/email/${template}.pug`,
       {
         firstName: this.firstName,
         url: this.url,
